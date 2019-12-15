70e55fb9ed0f6dafaaa75e045e3481968f60cc5d
Will MacCormack <wmac@Wills-MacBook-Pro.local>
2019-12-15 15:55:55 -0800

    accounts for the two types of updates

--- server.js
+++ server.js
@@ -3,6 +3,31 @@ const app = express();
 
 var last;
 
+function updateLast(current)
+{
+     if (current.mt == "5N1x31")
+     {
+          last.windspeedmph = current.windspeedmph;
+          last.baromin = current.baromin;
+          last.battery = current.battery;
+          last.rssi = current.rssi;
+
+          last.winddir = current.winddir;
+          last.rainin = current.rainin;
+          last.dailyrainin = current.dailyrainin;
+     }
+     elseif (current.mt == "5N1x38")
+     {
+          last.windspeedmph = current.windspeedmph;
+          last.baromin = current.baromin;
+          last.battery = current.battery;
+          last.rssi = current.rssi;
+
+          last.humidity = current.humidity;
+          last.tempf = current.tempf;
+     }
+}
+
 const bodyParser = require('body-parser');
 // ...
 // ...
@@ -16,10 +41,9 @@ app.get('/', function (req, res) {
 app.get('/weatherstation/updateweatherstation', function (req, res) {
      var dt = new Date();
      res.send('{\"localtime\":\"' + dt.toLocaleTimeString('it-IT') + '\"}');
-     // res.send("work pls");
-     // console.log(req.body);
      console.log(req.query);
-     last = req.query;
+
+     updateLast(req.query);
 })
 
 app.listen(3000, function () {
